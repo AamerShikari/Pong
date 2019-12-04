4c8d9b6996f0c0ea87dc6306e2038876b6fd0be6
Aamer Shikari <41989380+AamerShikari@users.noreply.github.com>
2019-12-03 22:29:48 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,14 @@
 --[[
-    GD50 2018
     Pong Remake
 
     -- Main Program --
 
-    Author: Colton Ogden
+    Author: Aamer Shikari and Colton Ogden
+    shikariaamer@gmail.com
     cogden@cs50.harvard.edu
 
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
+    Features two paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 2 points wins.
 
     This version is built to more closely resemble the NES than
     the original Pong machines or the Atari 2600 in terms of
